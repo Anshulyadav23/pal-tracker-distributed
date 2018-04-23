f4acbfef8a26ebd2f3f009faaa2cce1a83afc98f
Anshul <anshul7415@gmail.com>
2018-04-23 14:34:32 -0600

    Changes made to deploy

--- applications/registration-server/src/main/java/io/pivotal/pal/tracker/registration/App.java
+++ applications/registration-server/src/main/java/io/pivotal/pal/tracker/registration/App.java
@@ -18,7 +18,7 @@ import java.util.TimeZone;
     "io.pivotal.pal.tracker.restsupport",
     "io.pivotal.pal.tracker.projects",
     "io.pivotal.pal.tracker.users",
-    "io.pivotal.pal.tracker.registration"
+
 })
 public class App {
     public static void main(String[] args) {
